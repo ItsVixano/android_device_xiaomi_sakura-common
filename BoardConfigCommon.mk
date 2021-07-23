7c59668cebb85deeb7a7f4563b474e8b03072c2d
ItsVixano <vixanonew@gmail.com>
2021-07-23 16:43:18 +0200

    sakura-common: Inherit from msm8953-common
    Change-Id: I77b1739ae551ae2d07452b68cd77a81f975d4f95

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+TARGET_KERNEL_VERSION := 4.9
+
+# Inherit from common msm8953-common
+include device/xiaomi/msm8953-common/BoardConfigCommon.mk
+
+SAKURA_PATH := device/xiaomi/sakura-common
+
+# Display
+TARGET_SCREEN_DENSITY := 420
+
+# Inherit the proprietary files
+include vendor/xiaomi/sakura-common/BoardConfigVendor.mk
---
+++ sakura.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2017-2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from msm8953-common
+$(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
+
+# Screen density
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/sakura-common/sakura-common-vendor.mk)
